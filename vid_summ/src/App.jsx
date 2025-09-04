5f031b3cc285897ecc07a299d99f2f6429ae0f30
Alvaro <alvarogonzalezc4@gmail.com>
2025-09-03 23:50:30 -0300

    Agrego componente nav bar

---
+++ vid_summ/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+import NavBar from "./components/navBar";
+
+export default App;
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  );
+}
---
+++ vid_summ/src/components/navBar.jsx
@@ -0,0 +1,16 @@
+function NavBar() {
+  return (
+    <nav class="bg-indigo-800 p-4 text-slate-300 flex justify-between">
+      <div>
+        <h1 className="text-3xl font-bold">Video Summary Generator</h1>
+      </div>
+      <div>
+        <a href="#" className="text-slate-300 hover:underline">
+          Logout
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
